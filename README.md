2585975c681f5ef9d02aa8f1b8f7e3b2f7d8ac61
Naveen Shankhwal <shankhwal@gmail.com>
2020-04-02 02:52:59 +0530

    Commiting Spring Boot Multi Module Project Structure

---
+++ README.md
@@ -0,0 +1,40 @@
+## MoviesDB | Spritle Software, Chennai
+
+### Java Task | Spritle
+
+### Spritle Coding Test Project
+
+The project has served its purpose to test my knowleadge on Restful Services,Spring Framework, Spring Boot, Jpa/Hibernate and Relational Database.
+
+This Project is Spring Boot Multi Module Project and It contains three child modules Service [JAR], Core [JAR], and Store[JAR].
+
+1) Core contains all the DTOs and Models. 
+2) Services contains all the Service Interfaces, Service Implementations, Controllers and Rest Controllers.
+3) Store contains all the JPA CRUD Operations and JPA Pagination/Sorting related Repositories.
+
+
+### MySql Setup : 
+I am using MySql Database 5.7 Version in this Spring Boot Multi Module Project. 
+For testing this project you need to install MySql Community Edition 5.7 Version. 
+After installing create a user with credential details username=root and password=root. 
+And create a schema db with name "moviesdb".
+
+The Postgresql database can be setup to run in a Docker image. The steps for the setup can be found in the postgresql.sh file.
+Movie import
+
+To import movies a key needs to be provieded at signin. To get such a key according to this Faq
+Setup
+
+Postgresql 9.x or newer.
+
+Eclipse Oxygen JEE or newer.
+
+Install Eclipse Plugin 'Eclipse Wild Web Developer' of the Eclipse Marketplace.
+
+Maven 3.3.3 or newer.
+
+Nodejs 12.16.x or newer
+
+Npm 6.13.x or newer
+
+Angular Cli 9 or newer.
---
+++ core/src/main/java/com/spritle/moviesdb/dto/MovieDto.java
@@ -0,0 +1,71 @@
+package com.spritle.moviesdb.dto;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MovieDto {
+	private Long id;
+	private String overview;
+	@JsonProperty("release_date")
+	private Date releaseDate;
+	private String title;	
+	private Integer movieId;
+	@JsonProperty("genre_ids")
+	private int[] generes;
+	private List<CastDto> myCast = new ArrayList<>();
+	private List<GenereDto> myGenere = new ArrayList<>();
+	
+	public Integer getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(Integer movieId) {
+		this.movieId = movieId;
+	}
+	public List<CastDto> getMyCast() {
+		return myCast;
+	}
+	public List<GenereDto> getMyGenere() {
+		return myGenere;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getOverview() {
+		return overview;
+	}
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}	
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int[] getGeneres() {
+		return generes;
+	}
+	public void setGeneres(int[] generes) {
+		this.generes = generes;
+	}
+	public void setMyCast(List<CastDto> myCast) {
+		this.myCast = myCast;
+	}
+	public void setMyGenere(List<GenereDto> myGenere) {
+		this.myGenere = myGenere;
+	}	
+}
